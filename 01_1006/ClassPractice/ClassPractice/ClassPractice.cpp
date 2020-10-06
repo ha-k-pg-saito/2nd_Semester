c774df400953c5f084475c9ef92d57b4333cefdb
MSI\ぺんたそ <soryuotaku@gmail.com>
2020-10-06 11:06:35 +0900

    クラスの作り方の復習用プロジェクトの作成

---
+++ 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
@@ -0,0 +1,39 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+
+//Fighterクラス
+class Fighter
+{
+
+public:
+
+	int m_hp;
+	int m_str;
+	int m_mag;
+	int m_tec;
+	int m_spd;
+	int m_def;
+	int m_luck;
+	int m_mde;
+
+	//コンストラクタ
+	Fighter();
+	//デストラクタ
+	~Fighter();
+};
+
+Fighter::Fighter()
+{
+
+}
+
+Fighter::~Fighter()
+{
+
+}
+int main()
+{
+
+	system("pause");
+	return 0;
+}
