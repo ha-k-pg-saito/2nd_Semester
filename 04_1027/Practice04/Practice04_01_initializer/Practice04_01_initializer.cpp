7b66e12043a63468db8dbecce0469952e89b357b
MSI\ぺんたそ <soryuotaku@gmail.com>
2020-10-27 11:17:39 +0900

    イニシャライザの初期化準の注意点
    引数付きのコンストラクタのイニシャライザ
    基底クラスでイニシャライザを使う

--- 04_1027/Practice04/Practice04_01_initializer/Practice04_01_initializer.cpp
+++ 04_1027/Practice04/Practice04_01_initializer/Practice04_01_initializer.cpp
@@ -6,6 +6,7 @@ class CharBase
 {
 public:
     CharBase();
+    CharBase(int hp,int mp);
     ~CharBase();
 
     void PrintParam();
@@ -20,13 +21,21 @@ private:
 
 //コンストラクタでメンバイニシャライズによる初期化のやり方
 //クラス名::コンストラクタ（引数）:メンバ変数（初期値）:メンバ変数（初期値）
-
+//注意点：メンバイニシャライザは、変数の宣言順に呼び出される
+//　　　　イニシャライザに書いた順番ではない点に注意
 CharBase::CharBase() :Hp(100), Mp(10),HP_MAX(999)   //constメンバ変数もここでなら初期化できる
 {
     //従来の方法
     //Hp = 10;
     //Mp = 10;
     //HP_MAX=999;       //コンストラクタ内ではconst型のメンバ変数を初期化できない
+}
+
+CharBase::CharBase(int hp, int mp)
+    :Hp(hp)
+    ,Mp(mp)
+    ,HP_MAX(999)
+{
 }
 
 CharBase::~CharBase()
@@ -42,6 +51,24 @@ void CharBase::PrintParam()
     printf("HP_MAX=%d\n",HP_MAX);
 }
 
+//CharBaseを継承した派生クラスPlayer
+class Player :public CharBase
+{
+public:
+    Player();
+
+private:
+    int Money;
+};
+
+Player::Player()
+    :CharBase(200,50)
+    ,Money(50)
+    
+{
+}
+
+
 int main()
 {
     //データ型にもコンストラクタはある
@@ -51,4 +78,6 @@ int main()
     CharBase ch;
     ch.PrintParam();
     
+    Player player;
+    player.PrintParam();
 }
